fe836f7c9d6d8115addd41fc4d6127db92f1cf50
Kevin Anaya <kevinanaya278@gmail.com>
2024-08-07 15:31:00 -0500

    add chromedriver file

--- src/test/java/stepsdefinitions/CompleteFromStepDefinition.java
+++ src/test/java/stepsdefinitions/CompleteFromStepDefinition.java
@@ -21,56 +21,47 @@ public class CompleteFromStepDefinition {
 
     @When("the user enther the txtbox")
     public void theUserEntherTheTxtbox() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user click in the button")
     public void theUserClickInTheButton() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user double click in the second button")
     public void theUserDoubleClickInTheSecondButton() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user select radio button")
     public void theUserSelectRadioButton() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user select checkbox")
     public void theUserSelectCheckbox() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user select dropdown")
     public void theUserSelectDropdown() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user generate alert")
     public void theUserGenerateAlert() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @When("the user generate confirm alert")
     public void theUserGenerateConfirmAlert() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
     @Then("the user finish process")
     public void theUserFinishProcess() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 }
